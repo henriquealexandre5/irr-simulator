0c9d470dd00d4ae75128a3323f06e7c43318f23c
Henrique Alexandre <henrique.alexandre@LOFT-SPM-2085.local>
2021-04-03 12:41:05 -0300

    feat: first commit

---
+++ streamlit_app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from utils import UnitCashflow, waterfall_plot
+
+_, col_image, _ = st.beta_columns([1,1,1])
+col_image.image("loft_logo.png",unsafe_allow_html=True)
+
+#Título calculadora
+st.markdown(
+    "<h1 style='text-align: center; color: #ff774a;'>Calculadora de TIR</h1>",
+    unsafe_allow_html=True,
+)
+
+#Informações
+file = """
+<details>
+<summary>ℹ️ Informação</summary>
+
+- Para realizar os cálculos de TIR assumimos algumas premissas:
+    - Registro da escritura de compra acontece 1 mês após assinatura do contrato de compra
+    - Reforma começa imediatamente após a assinatura da escritura de compra
+    - Valor da reforma é distribuido igualmente entre os meses
+    - Escritura de venda é registrada 1 mês após a assinatura do contrato com o comprador
+    - No momento da assinatura dos contratos de compra e venda é pago e recebido, respectivamente, um sinal de 10%
+- ** Quer ter uma experiência personalizada? Vem falar com a gente! ** :orange_heart:
+
+</details>
+"""
+st.markdown(file,unsafe_allow_html=True)
+st.write("\n")
+
+#Inputs usados na calculadora (Texto, Valor mínimo, Valor máximo, Valor inicial)
+inputs = {"renovation_time": ("Tempo de reforma", 0, 12, 0),
+          "renovation_cost": ("Valor da reforma", 0, 10 ** 6, 0),
+          "community_fee": ("Condomínio", 0, 10 ** 5, 0),
+          "property_tax": ("IPTU", 0, 10 ** 5, 0),
+          "holding_period": ("Holding period (tempo entre as escrituras de compra e venda)", 0, 16, 0),
+          "selling_price": ("Valor de venda", 0, 10 ** 8, 0),
+          "buying_price": ("Valor de compra", 0, 10 ** 8, 0),
+}
+
+#Fazer campos lado a lado
+cols = {'renovation_time': 'col1',
+        'renovation_cost': 'col2',
+        'community_fee': 'col3',
+        'property_tax': 'col4'}
+
+col1, col2 = st.beta_columns(2)
+col3, col4 = st.beta_columns(2)
+
+for var, values in inputs.items():
+    if var in cols:
+        inputs[var] = eval(f'{cols[var]}.number_input' + str(values))
+    else:    
+        inputs[var] = eval('st.number_input' + str(values)) 
+
+#Resultados
+cashflow_size = 25
+inputs['downpayment'] = 0.1
+
+ap = UnitCashflow(inputs, len_cashflow = cashflow_size)
+
+st.subheader("TIR (% a.a)")
+if (inputs["buying_price"] == 0) | (inputs["selling_price"] == 0) | (inputs["holding_period"] == 0):
+    st.write('Adicione todos os valores para realizar o cálculo')
+else:
+    st.write(round(ap.get_irr(), 2))
+
+st.write('----')
+
+##Mostrar cashflows
+
+#Total
+waterfall_plot(ap.cashflow, "Cashflow total")
+
+# Buy
+with st.beta_expander("Cashflow de compra"):
+    waterfall_plot(ap.cashflows['buying_cashflow'])
+
+# Holding costs
+with st.beta_expander("Cashflow de holding costs"):
+    waterfall_plot(ap.cashflows['holding_costs_cashflow'])
+
+# Renovations
+with st.beta_expander("Cashflow de reforma"):
+    waterfall_plot(ap.cashflows['renovation_costs_cashflow'])
+
+# Sell
+with st.beta_expander("Cashflow de venda"):
+    waterfall_plot(ap.cashflows['selling_cashflow'])
---
+++ utils.py
@@ -0,0 +1,99 @@
+import numpy as np
+import plotly.graph_objects as go
+import streamlit as st
+import numpy_financial as npf
+
+
+def waterfall_plot(cashflow, label=""):
+    n = cashflow.shape[0]
+    fig = go.Figure(
+        go.Waterfall(
+            name="",
+            orientation="v",
+            measure=["absolute"] * n,
+            x=["Mês " + str(x) for x in range(n)],
+            textposition="outside",
+            y=cashflow,
+            connector={"line": {"color": "black"}},
+            totals = {"marker":{"color":"#ff774a", "line":{"color":"black", "width":1}}}
+        )
+    )
+    fig.update_layout(
+        title={"text": label, "x": 0.5, "xanchor": "center"}, showlegend=False,
+    )
+    
+    # fig.update_layout({
+    #     'plot_bgcolor': 'white',
+    #     'paper_bgcolor': 'white',
+    #     })
+    
+    st.plotly_chart(fig)
+
+
+class UnitCashflow:
+    def __init__(self, params, len_cashflow=25):
+        self.params = params
+        self.flows = [
+            "buying_cashflow",
+            "holding_costs_cashflow",
+            "renovation_costs_cashflow",
+            "selling_cashflow",
+        ]
+        self.init_cashflows(cashflow_size=len_cashflow)
+        self.consolidate_cashflows()
+
+    def init_cashflows(self, cashflow_size):
+        # Cashflow Total
+        self.cashflow = np.zeros(cashflow_size)
+        # Cashflow por fluxo
+        self.cashflows = {}
+        for flow in self.flows:
+            self.cashflows[flow] = np.zeros(cashflow_size)
+
+    def buying_cashflow(self):
+        # Pagamento do sinal em T = 0
+        self.cashflows["buying_cashflow"][0] = (
+            -self.params["downpayment"] * self.params["buying_price"]
+        )
+        # Pagamento do valor restante em T = 1
+        self.cashflows["buying_cashflow"][1] = (
+            -(1 - self.params["downpayment"]) * self.params["buying_price"]
+        )
+
+    def holding_costs_cashflow(self):
+        # Holding costs começa em T = 1 até T = HP - 1
+        # (começa a ser pago no momento da escritura de compra e vai até um mês antes da escritura de venda)
+        self.cashflows["holding_costs_cashflow"][1 : self.params["holding_period"]] -= (
+            self.params["community_fee"] + self.params["property_tax"]
+        )
+
+    def renovation_costs_cashflow(self):
+        # Reforma começa em T = 1 e vai até T = 1 + renovation_time
+        renovation_start = 1
+        renovation_end = renovation_start + self.params["renovation_time"]
+        # Valor da reforma é distribuido igualmente entre o tempo
+        self.cashflows["renovation_costs_cashflow"][
+            renovation_start:renovation_end
+        ] = -self.params["renovation_cost"] / max(self.params["renovation_time"], 1)
+
+    def selling_cashflow(self):
+        # Recebimento do sinal em T = HP - 1
+        self.cashflows["selling_cashflow"][self.params["holding_period"] - 1] = (
+            self.params["downpayment"] * self.params["selling_price"]
+        )
+        # Recebimento do valor restante em T = HP
+        self.cashflows["selling_cashflow"][self.params["holding_period"]] = (
+            1 - self.params["downpayment"]
+        ) * self.params["selling_price"]
+
+    def consolidate_cashflows(self):
+        self.buying_cashflow()
+        self.holding_costs_cashflow()
+        self.renovation_costs_cashflow()
+        self.selling_cashflow()
+
+        for flow in self.flows:
+            self.cashflow += self.cashflows[flow]
+
+    def get_irr(self):
+        return ((1 + npf.irr(self.cashflow)) ** (12) - 1) * 100
